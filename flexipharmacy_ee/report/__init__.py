eafbc4c12e2b493ccab0d38d38e120b009523260
Newmarketcompany <75623864+Newmarketcompany@users.noreply.github.com>
2021-10-30 03:48:18 +0300

    Add files via upload

---
+++ flexipharmacy_ee/report/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Acespritech Solutions Pvt. Ltd. (<www.acespritech.com>)
+# Copyright(c): 2012-Present Acespritech Solutions Pvt. Ltd.
+# All Rights Reserved.
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
+from . import sales_details_pdf_template
+from . import customer_sale_and_return
+from . import non_moving_stock_report
+from . import product_expiry_report_template
+from . import slow_moving_inventory
+from . import report
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
