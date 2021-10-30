f886040d4bc06af585d936dd8ea866c1cd332ab4
Newmarketcompany <75623864+Newmarketcompany@users.noreply.github.com>
2021-10-30 03:49:32 +0300

    Add files via upload

---
+++ flexipharmacy_ee/wizard/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Acespritech Solutions Pvt. Ltd. (<www.acespritech.com>)
+# Copyright(c): 2012-Present Acespritech Solutions Pvt. Ltd.
+# All Rights Reserved.
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
+from . import generate_product_barcode
+from . import non_moving_stock
+from . import product_expiry_report_wizard
+from . import slow_moving_inventory_wizard
+from . import pos_doctor_commission_payment
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
