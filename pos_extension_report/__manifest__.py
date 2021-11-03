a326512e76f5183dfe7d047dc9ba8d9122f1df69
Newmarketcompany <75623864+Newmarketcompany@users.noreply.github.com>
2021-11-03 04:02:25 +0300

    Add files via upload

---
+++ pos_extension_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'POS More Reports',
+    'version': '14.0',
+    'category': 'Point of sale',
+    'author': 'Bahey Wadea - Amek-Capital',
+    'website': 'https://www.amekcapital.com/',
+    'summary': 'Add POS Reports',
+    'description':"Add POS Reports",
+    'depends': ['web', 'point_of_sale'],
+    'data': ['report.xml',],
+    'qweb': [
+
+    ],
+    'installable': True,
+
+}
